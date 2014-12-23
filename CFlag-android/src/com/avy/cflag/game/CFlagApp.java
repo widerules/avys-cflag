c7dd708ae6d31a868448371e2d2f9b23fb134ef5
rssindian <rssindian@gmail.com>
2014-12-23 17:58:29 +0000

    Enabling ACRA - Android Crash Reporting

---
+++ CFlag-android/src/com/avy/cflag/game/CFlagApp.java
@@ -0,0 +1,34 @@
+package com.avy.cflag.game;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.acra.sender.HttpSender;
+
+import android.app.Application;
+
+	@ReportsCrashes(
+		formKey="",
+	    formUri = "https://avystudios.iriscouch.com/acra-cflag/_design/acra-storage/_update/report",
+	    reportType = HttpSender.Type.JSON,
+	    httpMethod = HttpSender.Method.POST,
+	    formUriBasicAuthLogin = "rssindian",
+	    formUriBasicAuthPassword = "narayan*28482",
+		mode = ReportingInteractionMode.DIALOG,
+		resToastText = R.string.crash_toast_text, 
+		resDialogText = R.string.crash_dialog_text,
+		resDialogIcon = android.R.drawable.ic_dialog_info, 
+		resDialogTitle = R.string.crash_dialog_title, 
+		resDialogCommentPrompt = R.string.crash_dialog_comment_prompt,
+		resDialogOkToast = R.string.crash_dialog_ok_toast
+	)
+
+
+public class CFlagApp extends Application {
+
+	  @Override
+	   public void onCreate() {
+	        super.onCreate();
+	        ACRA.init(this);
+	   }
+}
