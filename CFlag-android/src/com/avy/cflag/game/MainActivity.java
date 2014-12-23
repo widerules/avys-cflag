5aada858f295b66664985ef7b42800fe851f17b6
rssindian@gmail.com <rssindian@gmail.com@a567f1ce-8fad-2f60-29b0-8b2212edba39>
2014-12-23 17:58:29 +0000

    Enabling ACRA - Android Crash Reporting

--- CFlag-android/src/com/avy/cflag/game/MainActivity.java
+++ CFlag-android/src/com/avy/cflag/game/MainActivity.java
@@ -1,7 +1,10 @@
 package com.avy.cflag.game;
 
+import org.acra.ACRA;
+
 import android.os.Bundle;
 
+import com.avy.cflag.base.AcraMap;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
 
@@ -18,6 +21,12 @@ public class MainActivity extends AndroidApplication {
         cfg.useAccelerometer=false;
         cfg.useCompass=false;
         cfg.useWakelock=true;
-        initialize(new CFlagGame(), cfg);
+        initialize(new CFlagGame(new AcraMap() {
+			
+			@Override
+			public void putCustomData(String key, String value) {
+				ACRA.getErrorReporter().putCustomData(key, value);
+			}
+		}), cfg);
     }
 }
